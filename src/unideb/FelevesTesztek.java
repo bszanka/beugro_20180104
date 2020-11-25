437954ec8dbea9473fb221ca39cf27a5e12074a8
Balazs Szanka <szankabalazs@gmail.com>
2020-11-25 13:26:17 +0100

    #5 folyt

--- src/unideb/FelevesTesztek.java
+++ src/unideb/FelevesTesztek.java
@@ -1,4 +1,33 @@
 package unideb;
 
-public class FelevesTesztek {
+import egyetem.NincsTesztException;
+import egyetem.Szamonkeres;
+import egyetem.Tanulnivalok;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FelevesTesztek implements Tanulnivalok {
+    private String felevAzonosito;
+    private List <Szamonkeres> felevesTesztek = new ArrayList<>();
+
+    public FelevesTesztek(String felevAzonosito, List<Szamonkeres> felevesTesztek) {
+        this.felevAzonosito = felevAzonosito;
+        this.felevesTesztek = felevesTesztek;
+    }
+
+    @Override
+    public void hozzaad(Szamonkeres teszt) {
+
+    }
+
+    @Override
+    public double atlagPontszam() throws NincsTesztException {
+        return 0;
+    }
+
+    @Override
+    public List<Szamonkeres> tesztek(Boolean vizsgak, Boolean irasbelik) {
+        return null;
+    }
 }
